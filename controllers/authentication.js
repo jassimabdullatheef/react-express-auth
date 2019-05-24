248a4b9533883cf11619038f077d897c2e3b3ec7
Jassim Abdullatheef <jabdullatheef@stcs.com.sa>
2019-05-24 15:34:48 +0300

    sugnup completed

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,9 +1,21 @@
-const mongoose = require('mongoose')
+const jwt = require('jwt-simple')
 const User = require('../models/user')
+const config = require('../config')
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime()
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email
   const password = req.body.password
+
+  if (!email || !password) {
+    return res.status(422).send({
+      error: 'You must provide email and password'
+    })
+  }
 
   User.findOne({ email: email }, function(err, existingUser) {
     if (err) {
@@ -23,7 +35,7 @@ exports.signup = function(req, res, next) {
           return next(err)
         }
 
-        res.json({ success: true })
+        res.json({ token: tokenForUser(user) })
       })
     }
   })
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
 
 // Define model
 const userSchema = new Schema({
@@ -9,6 +10,25 @@ const userSchema = new Schema({
     lowercase: true
   },
   password: String
+})
+
+// on save, encrypt password
+userSchema.pre('save', function(next) {
+  const user = this
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err)
+    }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err)
+      }
+
+      user.password = hash
+      next()
+    })
+  })
 })
 
 const ModelClass = mongoose.model('user', userSchema)
