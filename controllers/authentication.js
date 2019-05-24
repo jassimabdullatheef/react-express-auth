08cbd31a20db24f04a69a6339ebe1a0d80337b1a
Jassim Abdullatheef <jabdullatheef@stcs.com.sa>
2019-05-24 15:58:50 +0300

    introduced authentication using passport

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -40,3 +40,14 @@ exports.signup = function(req, res, next) {
     }
   })
 }
+
+exports.login = function(req, res, next) {
+  const email = req.body.email
+  const password = req.body.password
+
+  if (!email || !password) {
+    return res.status(422).send({
+      error: 'You must provide email and password'
+    })
+  }
+}
